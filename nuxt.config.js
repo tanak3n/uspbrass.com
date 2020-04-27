2e184e8b1e8476eeb950811d39a2454edfcba6ee
tanak3n <33195549+tanak3n@users.noreply.github.com>
2020-04-28 01:39:05 +0900

    head の title を変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,8 +6,8 @@ export default {
    ** Headers of the page
    */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: '%s | 滋賀県立大学吹奏楽部',
+    // title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
