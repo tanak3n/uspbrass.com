d4ce75e495c2b70560066c2302e719d2bcf93987
tanak3n <33195549+tanak3n@users.noreply.github.com>
2020-05-05 02:58:51 +0900

    トップページを更新

--- pages/index.vue
+++ pages/index.vue
@@ -32,7 +32,58 @@
           </v-col>
         </v-row>
       </v-img>
-      <!-- これはトップページの内容 -->
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-card>
+            <v-card-title>ホームページをリニューアルしました</v-card-title>
+            <v-card-text
+              >5/5より吹奏楽部のホームページが新しくなりました。今後、演奏会の情報やパート紹介などのコンテンツを充実させていきます。これからもよろしくお願いいたします。</v-card-text
+            >
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-card>
+            <v-card-title>新入部員募集中！</v-card-title>
+            <v-card-text
+              >滋賀県立大学吹奏楽部は新入部員を絶賛募集中です。</v-card-text
+            >
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-card>
+            <v-btn class="float-right mt-4 mr-4">
+              <v-icon>mdi-twitter</v-icon>
+              Open Timeline
+            </v-btn>
+            <v-card-title>Twitter</v-card-title>
+            <a
+              class="twitter-timeline"
+              data-height="500"
+              href="https://twitter.com/uspbrass?ref_src=twsrc%5Etfw"
+              data-chrome="nofooter noheader"
+              >Tweets by uspbrass</a
+            >
+            <script
+              async
+              src="https://platform.twitter.com/widgets.js"
+              charset="utf-8"
+            ></script>
+          </v-card>
+        </v-col>
+        <v-col cols="12" md="8">
+          <v-card>
+            <v-card-title>アクセス</v-card-title>
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1628.9431528157104!2d136.21523965815865!3d35.259084185232744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzXCsDE1JzMyLjciTiAxMzbCsDEyJzU4LjgiRQ!5e0!3m2!1sja!2sjp!4v1554401176613!5m2!1sja!2sjp"
+              width="100%"
+              height="450"
+              frameborder="0"
+              style="border:0"
+              allowfullscreen
+            ></iframe>
+          </v-card>
+        </v-col>
+      </v-row>
     </v-container>
   </div>
 </template>
