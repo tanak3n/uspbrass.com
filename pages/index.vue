e73a9b7ed2436c4a2682fe47a7918b4a4eaefd7b
tanak3n <33195549+tanak3n@users.noreply.github.com>
2020-05-05 20:57:23 +0900

    トップページを変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,13 @@
 <template>
   <div>
-    <v-container fluid>
-      <v-alert type="info">
-        現在、新型コロナウイルスの影響で活動を休止しています。
-      </v-alert>
-      <v-img max-height="500px" position="50% 90%" src="/regular_concert.jpg">
+    <v-container fluid class="ma-0 pa-0">
+      <v-img height="100vh" position="50% 90%" src="/regular_concert.jpg">
         <v-row
           align="center"
           justify="center"
-          class="lightbox white--text pa-2 fill-height"
+          class="white--text mx-3 fill-height text-center"
         >
-          <v-col class="with-shadow" cols="12">
+          <v-col class="with-shadow my-1" cols="12">
             <h1 class="display-1 mb-4">
               University of Shiga Prefecture Wind Orchestra
             </h1>
@@ -32,30 +29,37 @@
           </v-col>
         </v-row>
       </v-img>
+    </v-container>
+    <v-container>
+      <v-alert type="info">
+        現在、新型コロナウイルスの影響で活動を休止しています。
+      </v-alert>
       <v-row>
         <v-col cols="12" md="6">
-          <v-card>
-            <v-card-title>ホームページをリニューアルしました</v-card-title>
-            <v-card-text
-              >5/5より吹奏楽部のホームページが新しくなりました。今後、演奏会の情報やパート紹介などのコンテンツを充実させていきます。これからもよろしくお願いいたします。</v-card-text
-            >
+          <v-card class="pa-3">
+            <h2 class="headline font-weight-light">
+              ホームページをリニューアルしました
+            </h2>
+            <p class="mt-4">
+              5/5より吹奏楽部のホームページが新しくなりました。今後、演奏会の情報やパート紹介などのコンテンツを充実させていきます。これからもよろしくお願いいたします。
+            </p>
           </v-card>
         </v-col>
         <v-col cols="12" md="6">
-          <v-card>
-            <v-card-title>新入部員募集中！</v-card-title>
-            <v-card-text
-              >滋賀県立大学吹奏楽部は新入部員を絶賛募集中です。</v-card-text
-            >
+          <v-card class="pa-3">
+            <h2 class="headline font-weight-light">
+              新入部員募集中！
+            </h2>
+            <p class="mt-4">
+              滋賀県立大学吹奏楽部は新入部員を絶賛募集中です。
+            </p>
           </v-card>
         </v-col>
         <v-col cols="12" md="4">
-          <v-card>
-            <v-btn class="float-right mt-4 mr-4">
-              <v-icon>mdi-twitter</v-icon>
-              Open Timeline
-            </v-btn>
-            <v-card-title>Twitter</v-card-title>
+          <v-card class="pa-3">
+            <h2 class="headline font-weight-light mb-4">
+              Twitter
+            </h2>
             <a
               class="twitter-timeline"
               data-height="500"
@@ -71,8 +75,10 @@
           </v-card>
         </v-col>
         <v-col cols="12" md="8">
-          <v-card>
-            <v-card-title>アクセス</v-card-title>
+          <v-card class="pa-3">
+            <h2 class="headline font-weight-light mb-4">
+              アクセス
+            </h2>
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1628.9431528157104!2d136.21523965815865!3d35.259084185232744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzXCsDE1JzMyLjciTiAxMzbCsDEyJzU4LjgiRQ!5e0!3m2!1sja!2sjp!4v1554401176613!5m2!1sja!2sjp"
               width="100%"
