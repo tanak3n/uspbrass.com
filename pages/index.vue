aa02196572aaafa7a47efd98bfb4cff6acd876d4
tanak3n <33195549+tanak3n@users.noreply.github.com>
2020-05-05 17:39:02 +0900

    地図の高さ変更

--- pages/index.vue
+++ pages/index.vue
@@ -76,7 +76,7 @@
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1628.9431528157104!2d136.21523965815865!3d35.259084185232744!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzXCsDE1JzMyLjciTiAxMzbCsDEyJzU4LjgiRQ!5e0!3m2!1sja!2sjp!4v1554401176613!5m2!1sja!2sjp"
               width="100%"
-              height="450"
+              height="500"
               frameborder="0"
               style="border:0"
               allowfullscreen
