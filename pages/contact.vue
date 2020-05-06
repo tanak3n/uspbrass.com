352475fd86edb6fdf83c4a1d1c35624238e42aba
tanak3n <33195549+tanak3n@users.noreply.github.com>
2020-05-06 16:22:49 +0900

    お問い合わせページに地図・住所を追加

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container>
     <h1>{{ title }}</h1>
-    <v-row>
-      <v-col sm="4" cols="12" align-self="stretch"
+    <v-row class="text-center" justify="center">
+      <v-col md="4" sm="6" cols="12" align-self="stretch"
         ><v-card tile height="100%" color="#1DA1F2" dark>
           <v-card-title
             ><v-icon large left>mdi-twitter</v-icon
@@ -20,7 +20,7 @@
           </v-card-actions></v-card
         ></v-col
       >
-      <v-col sm="4" cols="12" align-self="stretch"
+      <v-col md="4" sm="6" cols="12" align-self="stretch"
         ><v-card tile height="100%" color="purple" dark>
           <v-card-title
             ><v-icon large left>mdi-comment-question</v-icon
@@ -40,7 +40,7 @@
           </v-card-actions></v-card
         ></v-col
       >
-      <v-col sm="4" cols="12" align-self="stretch"
+      <v-col md="4" sm="6" cols="12" align-self="stretch"
         ><v-card tile height="100%" color="secondary" dark>
           <v-card-title
             ><v-icon large left>mdi-email</v-icon
@@ -59,6 +59,34 @@
           </v-card-actions></v-card
         ></v-col
       >
+    </v-row>
+    <h1>アクセス</h1>
+    <v-row>
+      <v-col md="5" cols="12">
+        <v-card tile height="100%">
+          <v-card-title>連絡先</v-card-title>
+          <v-card-text class="text-center">
+            〒522-0057<br />
+            滋賀県彦根市八坂町2500 滋賀県立大学内<br />
+            滋賀県立大学吹奏楽部
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col md="7" cols="12">
+        <v-card tile height="100%"
+          ><v-card-title>地図</v-card-title
+          ><iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d814.4739490479199!2d136.21604282924622!3d35.25884809385076!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMzXCsDE1JzMxLjkiTiAxMzbCsDEyJzU5LjciRQ!5e0!3m2!1sja!2sjp!4v1588748287601!5m2!1sja!2sjp"
+            width="100%"
+            height="450"
+            frameborder="0"
+            style="border:0;"
+            allowfullscreen=""
+            aria-hidden="false"
+            tabindex="0"
+          ></iframe
+        ></v-card>
+      </v-col>
     </v-row>
   </v-container>
 </template>
