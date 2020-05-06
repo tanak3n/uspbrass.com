4931590b043ca427598765152fcbad2a2b1dbafa
tanak3n <33195549+tanak3n@users.noreply.github.com>
2020-05-06 15:16:57 +0900

    GA Tracking Codeを追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,13 @@ export default {
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module',
-    '@nuxtjs/vuetify'
+    '@nuxtjs/vuetify',
+    [
+      '@nuxtjs/google-analytics',
+      {
+        id: 'UA-165694369-1'
+      }
+    ]
   ],
   /*
    ** Nuxt.js modules
