198a43ae1cde9b62cdd829e076ace33c1d8d5cac
tanak3n <33195549+tanak3n@users.noreply.github.com>
2020-08-02 03:44:17 +0900

    活動人数を変更

--- pages/index.vue
+++ pages/index.vue
@@ -119,7 +119,7 @@ export default {
         {
           icon: 'mdi-account-group',
           term: '活動人数',
-          description: '48人（B1: 4人，B2: 10人，B3: 14人，B4: 18人，M1: 2人）'
+          description: '49人（B1: 4人，B2: 10人，B3: 15人，B4: 18人，M1: 2人）'
         }
       ]
     }
